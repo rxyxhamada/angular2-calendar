e5b2c89ae2b44b8a63b0f26de374fb0128f9e83e
Yusuke Hamada <hamada_yusuke@r.recruit.co.jp>
2016-11-06 09:52:25 +0900

    feat: modify default interface for the first mock

---
+++ src/components/common/calendar-event.ts
@@ -0,0 +1,12 @@
+import { CalendarEvent as OriginalCalendarEvent  } from 'calendar-utils';
+
+export interface CalendarEvent extends OriginalCalendarEvent {
+
+    id?: number;
+
+    reserve?: any;
+
+    activated: boolean;
+
+    confirmed?: boolean;
+}
